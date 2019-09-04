8c757637699c7bb0f9f03070e63dd9a43e8ff389
Sodimac Usuario <sodimac.usuario1@gmail.com>
2019-09-04 09:04:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// desktop @1x
+import cd01 from "./src/01.jpg";
+import cd02 from "./src/02.jpg";
+import cd03 from "./src/03.jpg";
+import cd04 from "./src/04.jpg";
+import cd05 from "./src/05.jpg";
+import cd06 from "./src/06.jpg";
+import cd07 from "./src/07.jpg";
+import cd08 from "./src/08.jpg";
